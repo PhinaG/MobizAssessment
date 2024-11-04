92e21eb33376e5b679651ce0013bb2588b6b988c
PhinaG <phinadavis2812@gmail.com>
2024-11-04 23:09:40 +0200

    updated unit tests

--- employee-management-ts/src/tests/employees.test.ts
+++ employee-management-ts/src/tests/employees.test.ts
@@ -21,7 +21,7 @@ describe('Employee CRUD Operations', () => {
   });
 
   it('should read all employees', async () => {
-    const mockData: Employee[] = [{ id: '1', firstName: 'John Doe' }];
+    const mockData: Employee[] = [{ id: '1', firstName: 'Test Employee' }];
     (fs.readFile as unknown as jest.Mock).mockImplementation((path, callback) => {
       callback(null, Buffer.from(JSON.stringify(mockData)));
     });
@@ -32,7 +32,7 @@ describe('Employee CRUD Operations', () => {
   });
 
   it('should get an employee by ID', async () => {
-    const mockData: Employee[] = [{ id: '1', firstName: 'John Doe' }];
+    const mockData: Employee[] = [{ id: '1', firstName: 'Test Employee' }];
     (fs.readFile as unknown as jest.Mock).mockImplementation((path, callback) => {
       callback(null, Buffer.from(JSON.stringify(mockData)));
     });
@@ -43,8 +43,8 @@ describe('Employee CRUD Operations', () => {
   });
 
   it('should add a new employee', async () => {
-    const initialData: Employee[] = [{ id: '1', firstName: 'John Doe' }];
-    const newEmployee: Employee = { id: '2', firstName: 'Jane Doe' };
+    const initialData: Employee[] = [{ id: '1', firstName: 'Test Employee' }];
+    const newEmployee: Employee = { id: '2', firstName: 'Test Employee' };
 
     (fs.readFile as unknown as jest.Mock).mockImplementation((path, callback) => {
       callback(null, Buffer.from(JSON.stringify(initialData)));
@@ -56,22 +56,11 @@ describe('Employee CRUD Operations', () => {
 
     const res = await request(app).post('/employees').send(newEmployee);
     expect(res.status).toBe(200);
-    // expect(fs.writeFile).toHaveBeenCalledWith(
-    //   expect.any(String),
-    //   JSON.stringify([
-    //     { id: '1', name: 'John Doe' },
-    //     expect.objectContaining({
-    //       id: '2',
-    //       name: 'Jane Doe'
-    //     })
-    //   ]),
-    //   expect.any(Function)
-    // );
   });
 
   it('should update an existing employee', async () => {
-    const initialData: Employee[] = [{ id: '1', firstName: 'John Doe' }];
-    const updatedEmployee: Employee = { id: '1', firstName: 'John Smith' };
+    const initialData: Employee[] = [{ id: '1', firstName: 'Test Employee' }];
+    const updatedEmployee: Employee = { id: '1', firstName: 'Test Employee 2' };
 
     (fs.readFile as unknown as jest.Mock).mockImplementation((path, callback) => {
       callback(null, Buffer.from(JSON.stringify(initialData)));
@@ -83,20 +72,10 @@ describe('Employee CRUD Operations', () => {
 
     const res = await request(app).put('/employees/1').send(updatedEmployee);
     expect(res.status).toBe(200);
-    // expect(fs.writeFile).toHaveBeenCalledWith(
-    //   expect.any(String),
-    //   JSON.stringify([
-    //     expect.objectContaining({
-    //       id: '1',
-    //       name: 'John Smith'
-    //     })
-    //   ]),
-    //   expect.any(Function)
-    // );
   });
 
   it('should delete an employee', async () => {
-    const initialData: Employee[] = [{ id: '1', firstName: 'John Doe' }];
+    const initialData: Employee[] = [{ id: '1', firstName: 'Test Employee' }];
 
     (fs.readFile as unknown as jest.Mock).mockImplementation((path, callback) => {
       callback(null, Buffer.from(JSON.stringify(initialData)));
